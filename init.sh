a4ba5fa6bfb13a30f2a82b96ff34e7d82a706e63
joel <jrevertvila@gmail.com>
2020-12-02 21:45:09 +0100

    add init script

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
-#if [ "$EUID" -ne 0 ]
+#!/bin/bash
+# if [ "$EUID" -ne 0 ]
 #  then echo "Please run as root"
 #  exit
-#fi
-gnome-terminal --working-directory="~./Servidor20_21/go_projects/src/github.com/revand/App_Go_Larave_Angular_TEST/frontend/angular" -- ng serve --open
+# fi
+# gnome-terminal --working-directory="~./Servidor20_21/go_projects/src/github.com/revand/App_Go_Larave_Angular_TEST/frontend/angular" -- ng serve --open
 
-eval "$(./backend/go/start.sh)"
+# eval "$(./backend/go/start.sh)"
+
+sudo service apache2 stop
+echo "Stopping apache2 -`tput setaf 2;` OK`tput sgr 0`"
+sudo service mysql stop
+echo "Stopping mysql -`tput setaf 2;` OK`tput sgr 0`"
+sudo docker-compose up
