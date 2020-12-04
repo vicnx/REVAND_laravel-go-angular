bac77ac8dc4ebc1908e39718cdd881cc50a63a2e
jrevertvila <jrevertvila@gmail.com>
2020-12-04 10:57:37 +0100

    redis/v8 implemented docker compose

--- backend/go/redis/models.go
+++ backend/go/redis/models.go
@@ -1,36 +1,36 @@
-// package redis
+package redis
 
-// import (
-// 	"github.com/go-redis/redis"
-// )
+import (
+	"github.com/go-redis/redis/v8"
+)
 
-// //estructura de datos guardados, clave-valor
-// type Info struct {
-// 	Key   string `json:"key"   binding:"required"`
-// 	Value string `json:"value" binding:"required"`
-// }
+//estructura de datos guardados, clave-valor
+type Info struct {
+	Key   string `json:"key"   binding:"required"`
+	Value string `json:"value" binding:"required"`
+}
 
-// //creamos el cliente redis
-// func newClient() *redis.Client {
-// 	client := redis.NewClient(&redis.Options{
-// 		Addr:     "redis:6379",
-// 		Password: "",
-// 		DB:       0,
-// 	})
-// 	return client
-// }
+//creamos el cliente redis
+func newClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+	return client
+}
 
-// //guardar
-// func set(key string, value string, client *redis.Client) error {
-// 	err := client.Set(key, value, 0).Err()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
+//guardar
+func set(key string, value string, client *redis.Client) error {
+	err := client.Set(key, value, 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-// //obtener
-// func get(key string, client *redis.Client) (error, string) {
-// 	val, err := client.Get(key).Result()
-// 	return err, val
-// }
+//obtener
+func get(key string, client *redis.Client) (error, string) {
+	val, err := client.Get(key).Result()
+	return err, val
+}
--- backend/go/redis/routers.go
+++ backend/go/redis/routers.go
@@ -1,99 +1,99 @@
 // package redis
 
 // import (
-// 	_"fmt"
-// 	_"net/http"
+// 	"fmt"
+// 	"net/http"	
 
-// 	_"reflect"
+// 	"reflect"
 
 // 	"github.com/gin-gonic/gin"
 // )
 
 // func Routers(router *gin.RouterGroup) {
 // 	router.GET("/", getAll)
-// 	// router.GET("/:key", getOne)
-// 	// router.POST("/", save)
-// 	// router.DELETE("/:key", delete)
+// 	router.GET("/:key", getOne)
+// 	router.POST("/", save)
+// 	router.DELETE("/:key", delete)
 // }
 
+// //obtenemos todos los libros favoritos
 // func getAll(c *gin.Context) {
 
-// 	// client := newClient()
-// 	// var array map[string]string
-// 	// array = make(map[string]string)
+// 	client := newClient()
+// 	var array map[string]string
+// 	array = make(map[string]string)
 
-// 	// keys, err := client.Do("KEYS", "*").Result()
-// 	// //si error
-// 	// if err != nil {
-// 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// }
-// 	// //obtenemos los valores de las claves
-// 	// for i := 0; i < reflect.ValueOf(keys).Len(); i++ {
-// 	// 	key := fmt.Sprintf("%v", reflect.ValueOf(keys).Index(i)) // convert from interface to string
+// 	keys, err := client.Do("KEYS", "*").Result()
+// 	//si error
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 	}
+// 	//obtenemos los valores de las claves
+// 	for i := 0; i < reflect.ValueOf(keys).Len(); i++ {
+// 		key := fmt.Sprintf("%v", reflect.ValueOf(keys).Index(i)) // convert from interface to string
 
-// 	// 	err, val := get(key, client)
-// 	// 	if err != nil {
-// 	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// 	}
-// 	// 	array[key] = val
-// 	// }
-// 	// c.JSON(200, gin.H{"keys": array})
-// 	c.JSON(200, gin.H{"MANGOS": "getall"})
+// 		err, val := get(key, client)
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		}
+// 		array[key] = val
+// 	}
+// 	c.JSON(200, gin.H{"keys": array})
 // }
 
 // //obtenemos un libro favorito, especificando la clave
 // func getOne(c *gin.Context) {
-// 	// client := newClient()
+// 	client := newClient()
 
-// 	// key := c.Param("key")
+// 	key := c.Param("key")
 
-// 	// err, val := get(key, client)
-// 	// if err != nil {
-// 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// 	return
-// 	// }
+// 	err, val := get(key, client)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
 
-// 	// c.JSON(200, gin.H{key: val})
+// 	c.JSON(200, gin.H{key: val})
 
 // }
 
 // //borramos un libro favorito
 // func delete(c *gin.Context) {
-// 	// //creamos el cliente
-// 	// client := newClient()
-// 	// //le pasamos la clave
-// 	// key := c.Param("key")
-// 	// //borramos la clave
-// 	// n, err := client.Del(key).Result()
-// 	// //si error
-// 	// if err != nil {
-// 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// 	return
-// 	// }
-// 	// //si ok, mostramos un mensaje
-// 	// c.JSON(200, gin.H{"result": n})
+// 	//creamos el cliente
+// 	client := newClient()
+// 	//le pasamos la clave
+// 	key := c.Param("key")
+// 	//borramos la clave
+// 	n, err := client.Del(key).Result()
+// 	//si error
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	//si ok, mostramos un mensaje
+// 	c.JSON(200, gin.H{"result": n})
 
 // }
 
 // //guardamos los libros favoritos
 // func save(c *gin.Context) {
-// 	// client := newClient()
+// 	client := newClient()
 
-// 	// var info Info
-// 	// if err := c.ShouldBindJSON(&info); err != nil { //marca error, pero funciona
-// 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// 	return
-// 	// }
+// 	var info Info
+// 	if err := c.ShouldBindJSON(&info); err != nil { //marca error, pero funciona
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
 
-// 	// //pasamos los datos a json
-// 	// c.BindJSON(&info)
+// 	//pasamos los datos a json
+// 	c.BindJSON(&info)
 
-// 	// //si error
-// 	// err := set(info.Key, info.Value, client)
-// 	// if err != nil {
-// 	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 	// 	return
-// 	// }
-// 	// //si se guardan los datos correctamente
-// 	// c.JSON(200, gin.H{"result": "ok"})
+// 	//si error
+// 	err := set(info.Key, info.Value, client)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	//si se guardan los datos correctamente
+// 	c.JSON(200, gin.H{"result": "ok"})
 // }
